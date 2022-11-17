cccb45f2c2d6038199405e3f0d279d0704dcb6f6
Ernest Leung <ernestleung52@gmail.com>
2022-11-16 23:38:55 -0800

    fairlife

---
+++ start/retrieveCodes.js
@@ -0,0 +1,10 @@
+const GmailApi = require("../src/api/services/gmailAPI");
+
+
+const emailAddress = 'ernestfrwd01'
+
+const retrieveCodes = (profile, settlement) => {
+  const catchallGmail = new GmailApi(emailAddress);
+  
+  // const email = 
+}
