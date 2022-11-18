4bc2870252d482906860d0f100f32ac698fa31af
Ernest Leung <ernestleung52@gmail.com>
2022-11-17 22:40:34 -0800

    restructured settlement config file

--- config/settlement/index.js
+++ config/settlement/index.js
@@ -1,28 +1,20 @@
-/***************************************************************
- * Verify Email
- ****************************************************************/
-
-// -------- Company Info -------
-const weedKiller = {
-	name: 'Weed Killer',
-	verify: {
-		subject: 'Gilmore v. Monsanto',
-		regex: /(?<=\bis:\s)(\w+)/g,
+module.exports = companies = {
+	// Weedkiller Settlement
+	WEEDKILLER: {
+		verify: {
+			subject: 'Gilmore v. Monsanto',
+			regex: /(?<=\bis:\s)(\w+)/g,
+		},
+		redeem: {
+			regex: /[]/g,
+		},
 	},
-	redeem: {
-		regex: /[]/g,
-	},
-};
 
-const neon = {
-	name: 'Neon Makeup',
-	redeem: {
-		from: 'neonobsessionssettlement@hawkmarketplace.com',
-		regex: /(?<=\bcode:\s)(\w+)/g,
+	// Neon Settlement
+	NEON: {
+		redeem: {
+			from: 'neonobsessionssettlement@hawkmarketplace.com',
+			regex: /(?<=\bcode:\s)(\w+)/g,
+		},
 	},
-};
-
-module.exports = {
-	weedKiller,
-	neon,
 };
