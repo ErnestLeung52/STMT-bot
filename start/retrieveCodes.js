13fd18af81e1e2a45f0e9f8941548fee996d891e
Ernest Leung <ernestleung52@gmail.com>
2022-11-17 22:41:52 -0800

    added new starting script file for different functionalities

--- start/retrieveCodes.js
+++ start/retrieveCodes.js
@@ -1,10 +1,24 @@
-const GmailApi = require("../src/api/services/gmailAPI");
+const GmailApi = require('../src/api/services/gmailAPI');
+const company = require('../config/settlement/index');
 
+/************* Manual Input *************/
+const emailAddress = 'ernestfrwd01';
+const settlement = company.NEON;
 
-const emailAddress = 'ernestfrwd01'
+const retrieveCodes = async () => {
+	// Initialize gmail client API
+	const gmailClient = new GmailApi(emailAddress);
 
-const retrieveCodes = (profile, settlement) => {
-  const catchallGmail = new GmailApi(emailAddress);
-  
-  // const email = 
-}
+	// Search for redeem codes matching settlement email pattern
+	const emailIds = await gmailClient.searchEmailIds(settlement);
+
+	const redeemCodes = await gmailClient.extractEmailCodes(
+		'Redeem',
+		emailIds,
+		settlement
+	);
+
+	console.log(redeemCodes);
+};
+
+retrieveCodes();
