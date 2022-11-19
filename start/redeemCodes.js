5c622c8f48c998a1a724bdf96ca3a3d93eb554fb
Ernest Leung <ernestleung52@gmail.com>
2022-11-19 15:10:26 -0800

    redeem process script

---
+++ start/redeemCodes.js
@@ -0,0 +1,8 @@
+// redirect to myprepaidcard center
+// Solve fun Captcha
+// Wait to redirect to form page
+// Enter information
+// Activate
+// Extract Credit card on new model
+// Optional * Add to profile - sign in to add to existing accounts
+
